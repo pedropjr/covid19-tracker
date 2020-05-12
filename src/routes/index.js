3d309ac9a6facdcdd2f6cf2e2fde38c493fb4b83
Pedro Borges <ppmbjr@gmail.com>
2020-05-12 10:10:53 -0300

    refactor: testing netlify build settings

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Switch } from 'react-router-dom';
+import { Switch, Redirect } from 'react-router-dom';
 
 import Route from './Route';
 
 import Brasil from '~/pages/Brasil';
+import World from '~/pages/World';
+import About from '~/pages/About';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/brasil" component={Brasil} />
+      <Route path="/brasil" exact component={Brasil} />
+      <Route path="/mundo" exact component={World} />
+      <Route path="/sobre" exact component={About} />
+      <Redirect from="*" to="/brasil" />
     </Switch>
   );
 }
